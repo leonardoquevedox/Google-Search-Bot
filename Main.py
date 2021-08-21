6654cc791aa85e66c4b413751f008a81cd405514
Leonardo Quevedo <leonardoq@trin.ca>
2021-08-21 00:47:37 -0300

    🔥 Initial commit.

---
+++ Main.py
@@ -0,0 +1,55 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import time
+import datetime
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.common import exceptions
+from selenium.webdriver.common.keys import Keys
+import os
+
+# Initializes Gecko driver
+driver = webdriver.Firefox()
+links=["leopq", "leonardoquevedox", "pacleo"]
+
+def navigate_to_google():
+    # Navigates into google main page
+    driver.get('https://google.com')
+
+def type_search_keyword():
+    try:
+        # --- Declares script variables
+        search_term="Leonardo Quevedo"
+
+        # --- Retrieves the search input
+        search_input = driver.find_element_by_css_selector('input[type="text"]')
+        print("Found the search input...")
+
+        # --- Types search term into the search input
+        search_input.send_keys(search_term)
+        print("Filled it with the search term...")
+
+        # --- Submits the search form
+        search_input.send_keys(Keys.RETURN)
+        print("Submitted the search form.")
+
+    except Exception as e:
+        print("💥 Whoops! There was an failure executing the search step!")
+        print(e)
+
+def open_links_on_new_tabs():
+    # --- Looks for provided links
+    link = driver.find_element_by_xpath("//a[contains(text(),'leopq')]")
+    # --- Link 
+    print(link)
+
+def main():
+    # --- Navigates into Google page
+    navigate_to_google()
+    # --- Types search keyword
+    type_search_keyword()
+    # --- Waits for 5 seconds
+    time.sleep(5)
+
+main()
